e7aa23c5c36862ada1b6fef32e36e7045ca7fc3f
Krishna Addepalli <coolkrishna31@gmail.com>
2023-09-02 12:52:28 +0200

    Add support for property value injection into rest controllers

--- mycoolapp/src/main/java/com/learning/springboot/mycoolapp/rest/FunRestController.java
+++ mycoolapp/src/main/java/com/learning/springboot/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,16 @@
 package com.learning.springboot.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
     @GetMapping("/")
     String sayHello() {
         return "Hello World!";
@@ -18,5 +24,10 @@ public class FunRestController {
     @GetMapping("/fortune")
     String getDailyFortune() {
         return "Today is your lucky day";
+    }
+
+    @GetMapping("/teaminfo")
+    String getTeamInfo() {
+        return "Coach: " + coachName + ", Team name: " + teamName;
     }
 }
