772b5a180553f10eda4d4f1bf88978d90dcc5039
Krishna Addepalli <coolkrishna31@gmail.com>
2023-09-03 19:46:52 +0200

    Add @PostConstruct and @PreDestroy annotation example

--- mycoolapp/src/main/java/com/learning/springboot/util/CricketCoach.java
+++ mycoolapp/src/main/java/com/learning/springboot/util/CricketCoach.java
@@ -1,15 +1,27 @@
 package com.learning.springboot.util;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach {
 
     public CricketCoach() {
         System.out.println("In Constructor: " + getClass().getSimpleName());
+    }
+
+    @PostConstruct
+    void doMyStartupStuff() {
+        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    void doMyCleanupStuff() {
+        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
     }
 
     @Override
