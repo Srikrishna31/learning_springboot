2d6a7befb37269b43c7d8c5311f1d541f2a1cbca
Krishna Addepalli <coolkrishna31@gmail.com>
2023-09-08 12:53:22 +0200

    Add exception messages for outof bounds input or bad input

--- mycoolapp/src/main/java/com/learning/springboot/mycoolapp/rest/StudentRestController.java
+++ mycoolapp/src/main/java/com/learning/springboot/mycoolapp/rest/StudentRestController.java
@@ -2,6 +2,9 @@ package com.learning.springboot.mycoolapp.rest;
 
 import com.learning.springboot.mycoolapp.entity.Student;
 import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +34,31 @@ public class StudentRestController {
 
     @GetMapping("/students/{studentId}")
     Student getStudent(@PathVariable int studentId) {
+        if (studentId < 0 || studentId > theStudents.size()) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
         return theStudents.get(studentId);
+    }
+
+    @ExceptionHandler
+    ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException e) {
+        var error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(e.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    ResponseEntity<StudentErrorResponse> handleException(Exception e) {
+        var error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(e.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
