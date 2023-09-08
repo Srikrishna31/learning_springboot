bd02422ee0f869770ffcd1c141140de5ced08250
Krishna Addepalli <coolkrishna31@gmail.com>
2023-09-08 22:33:44 +0200

    Add support to update an employee

--- mycoolapp/src/main/java/com/learning/springboot/mycoolapp/rest/EmployeeRestController.java
+++ mycoolapp/src/main/java/com/learning/springboot/mycoolapp/rest/EmployeeRestController.java
@@ -5,6 +5,9 @@ import com.learning.springboot.mycoolapp.entity.Employee;
 import com.learning.springboot.mycoolapp.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +27,25 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public Optional<List<Employee>> findAll() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    Employee getEmployee(@PathVariable int employeeId) {
+        var theEmployee = employeeService.findById(employeeId);
+
+        if (theEmployee.isEmpty()) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return theEmployee.get();
+    }
+
+    @PostMapping("/employees")
+    Employee addEmployee(@RequestBody Employee theEmployee) {
+        // also just in case they pass an id in JSON ... set id to 0
+        // this is to force a save of new item ... instead of update
+        theEmployee.setId(0);
+
+        return employeeService.save(theEmployee);
     }
 }
