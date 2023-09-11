0c027951a27686e5c235fee0d79203c8a19414fe
Krishna Addepalli <coolkrishna31@gmail.com>
2023-09-11 12:39:08 +0200

    Use Spring Data REST library with no coding for exposing CRUD rest endpoints

--- mycoolapp/src/main/java/com/learning/springboot/mycoolapp/dao/EmployeeRepository.java
+++ mycoolapp/src/main/java/com/learning/springboot/mycoolapp/dao/EmployeeRepository.java
@@ -3,6 +3,8 @@ package com.learning.springboot.mycoolapp.dao;
 
 import com.learning.springboot.mycoolapp.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
